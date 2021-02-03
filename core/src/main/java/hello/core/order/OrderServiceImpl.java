003464cc321091cbd357117030c95970af67b93e
HyunHub <19924___@naver.com>
2021-02-04 00:28:30 +0900

    AppConfig - DIP, OCP

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,11 +9,11 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository =
-            new MemoryMemberRepository();
+/*    private final MemberRepository memberRepository =
+            new MemoryMemberRepository();*/
 
    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+   // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     /*=> 이렇게 되면 OrderServiceImpl는 DiscountPolicy(추상.인터페이스)에도 의존하고 있고
         FixDiscountPolicy RateDiscountPolicy (구체.구현 클래스)에도 의존하고 있어서
         DIP를 위반하는 것. 구체에 의존하지 말고 항상 추상에 의존하라
@@ -21,6 +21,19 @@ public class OrderServiceImpl implements OrderService{
         또한 RateDiscountPolicy로 변경하는 순간 소스코드를 수정해야 함.
         OCP 위반임.
         */
+
+/*    이렇게 하면 interface에만 의존
+    private DiscountPolicy discountPolicy;*/
+
+    //AppConfig에서 해주기 위해서. final은 값을 할당받든 생성자를 만들든 해야 함
+    //구체적인 class에 대해서 전혀모르고 interface에만 의존함. DIP 지킴.
+    public final MemberRepository memberRepository;
+    public final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
