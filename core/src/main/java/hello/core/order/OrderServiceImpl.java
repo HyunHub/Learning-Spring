7f32e4ea3a7cc0f4f00d6704e1c932987155e02f
HyunHub <19924___@naver.com>
2021-02-03 17:17:14 +0900

    Initialization commit

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository =
+            new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        // 단일 체계 명령이 잘 된 것. order는 discount 부분은 모름. discount만 수정
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
