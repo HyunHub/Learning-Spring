ced64bab1b6152fe482a96a301be0b46a2e56fc1
HyunHub <19924___@naver.com>
2021-02-03 17:42:45 +0900

    Initialization commit

--- core/src/main/java/hello/core/order/Order.java
+++ core/src/main/java/hello/core/order/Order.java
@@ -2,7 +2,7 @@ package hello.core.order;
 
 public class Order {
 
-    private  Long memberId;
+    private Long memberId;
     private String itemName;
     private int itemPrice;
     private int discountPrice;
