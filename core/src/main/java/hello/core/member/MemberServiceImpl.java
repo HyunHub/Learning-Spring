322e9600f19e6f4ee51528ee14bb68ba0a2a5cd7
HyunHub <19924___@naver.com>
2021-02-04 00:27:34 +0900

    AppConfig - DIP, OCP

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,19 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository =
+    /*private final MemberRepository memberRepository =
             new MemoryMemberRepository();
+            MemberServiceImpl가 MemoryMemberRepository()를 직접 지정해줌
+      근데 이렇게 하면 안 되니까 AppConfig에서 다 해줄 것*/
+
+    private final MemberRepository memberRepository;
+
+    // 생성자를 통해서 이 MemberRepository에 구현체가 뭐가 들어갈지를 config
+    // 이렇게 되면 MemeberServiceImpl에는 MemberRepository라는 interface만 있음. 추상화에만 의존. DIP를 지킴.
+    //
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
