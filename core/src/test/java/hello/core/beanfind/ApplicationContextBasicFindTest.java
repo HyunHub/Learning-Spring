7eedab4ebdee7a304651d3b5b16949987be6b390
HyunHub <19924___@naver.com>
2021-02-11 20:12:10 +0900

    ApplicationContextBasicFindTest

--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,9 +6,11 @@ import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
 
@@ -36,5 +38,22 @@ public class ApplicationContextBasicFindTest {
     }
 
     // 위에는 interface로 조회해서 interface의 구현체가 대상이 됨.
+
+    @Test
+    @DisplayName("구체 타입으로 조회") // 유연성이 떨어짐
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+//        ac.getBean("xxxx", MemberService.class);
+//        MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        // ac 어쩌고 로직을 실행을 하면 왼쪽에 있는 예외가 터져야 한다. 터지면 성공
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
 
 }
