7734c20d8093ce358d023908ce6538ca393d1bea
HyunHub <19924___@naver.com>
2021-02-16 23:02:23 +0900

    Configuration Singleton test

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -4,8 +4,11 @@ import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ConfigurationSingletonTest {
 
@@ -16,12 +19,27 @@ public class ConfigurationSingletonTest {
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
 
-        MemberRepository memberRepository = memberService.getMemberRepository();
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
-        MemberRepository memberRepository1 = orderService.getMemberRepository();
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
 
-        System.out.println("memberService -> memberRepository = " + memberRepository);
-        System.out.println("orderService -> memberRepository = " + memberRepository1);
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        // 세 개 다 같게 나옴
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+    }
+
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass()); // class 타입이 뭔지
 
     }
 }
