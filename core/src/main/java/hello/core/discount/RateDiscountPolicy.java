ff06052d457aaa1259a0420dc729f8541e85a198
HyunHub <19924___@naver.com>
2021-02-20 01:19:05 +0900

    OrderServiceImpl - Qualifier Annotation

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy") // 이름 부여
-@Primary // 여러개의 Bean 중에 우선권 부여
+//@Primary // 여러개의 Bean 중에 우선권 부여
+@MainDiscountPolicy // 만든 annotation 넣기
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
