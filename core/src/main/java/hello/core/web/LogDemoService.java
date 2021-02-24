f5f32c4d39ebd706202f457c9e535a81233cd040
HyunHub <19924___@naver.com>
2021-02-25 01:10:16 +0900

    LogDemoService - ScopedProxyMode.TARGET_CLASS

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -11,9 +11,12 @@ public class LogDemoService {
 
 //    private final MyLogger myLogger;
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    private final MyLogger myLogger;
+
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
